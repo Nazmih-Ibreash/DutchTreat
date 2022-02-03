11a23bc41a90835544e9c144f8ca46c8430bce06
Nazmih-Ibreash <nazmihibreash@gmail.com>
2022-02-03 17:16:12 +0200

    mvc

--- Controllers/AppController.cs
+++ Controllers/AppController.cs
@@ -1,11 +1,42 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Dutch.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
 
-namespace Dutch.wwwroot
+namespace Dutch.Controllers
 {
     public class AppController : Controller
     {
         public IActionResult Index()
         {
+            return View();
+        }
+
+        [HttpGet("contact")]
+        public IActionResult Contact()
+        {
+            ViewBag.Title = "Contact Us";
+            return View();
+        }
+        [HttpPost("contact")]
+        public IActionResult Contact(ContentViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                //send email
+                ViewBag.Title = "Contact Us";
+            }
+            else
+            {
+                //show the errors
+            }
+            return View();
+
+        }
+
+        [HttpGet("about")]
+        public IActionResult About()
+        {
+            ViewBag.Title = "Abou Us";
             return View();
         }
     }
--- Startup.cs
+++ Startup.cs
@@ -16,20 +16,31 @@ namespace Dutch
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+            services.AddControllersWithViews().AddRazorRuntimeCompilation();
+            services.AddRazorPages();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/error");
+            }
+
             //app.UseDefaultFiles();
             app.UseStaticFiles();  
 
             app.UseRouting();
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapRazorPages();
                 endpoints.MapControllerRoute("Default", "/{controller}/{action}/{id?}",
-                    new { controller = "App", action ="index"});
+                    new { controller = "App", action ="Index"});
             });
         }
     }
