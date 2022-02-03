86487f89ec3cccfd2a0157c63c193016cb3a93aa
Nazmih-Ibreash <nazmihibreash@gmail.com>
2022-02-03 09:41:10 +0200

    Add project files.

---
+++ Controllers/AppController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Dutch.wwwroot
+{
+    public class AppController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ wwwroot/js/index.js
@@ -0,0 +1,23 @@
+﻿$(document).ready(function () {
+
+    var theForm = $("#theForm");
+    theForm.hide();
+
+    var button = $("#buybutton");
+    button.on("click", function () {
+        console.log('Buying Item Done!');
+    });
+
+    var productInfo = $(".product-info li");
+    productInfo.on("click", function () {
+        console.log("you clicked on " + $(this).text());
+    });
+
+
+    var $loginToggle = $("#loginToggle");
+    var $popupForm = $(".popup-form");
+
+    $loginToggle.on("click", function () {
+        $popupForm.toggle(1000);
+    });
+});
