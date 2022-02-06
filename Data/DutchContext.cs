c3f75a6b54f62d32343286f93d5a3f37d4247044
Nazmih-Ibreash <nazmihibreash@gmail.com>
2022-02-06 16:30:52 +0200

    startwithdatabase

---
+++ Data/DutchContext.cs
@@ -0,0 +1,11 @@
+﻿using DutchTreat.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Dutch.Data
+{
+    public class DutchContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+    }
+}
